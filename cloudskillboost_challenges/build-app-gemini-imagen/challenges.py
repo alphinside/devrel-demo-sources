d7c81168dc28d52b7ac1e291e372b00467370122
Alvin Prayuda <alvinprayuda@gmail.com>
2025-01-30 16:04:37 +0700

    chore: update styling

--- cloudskillboost_challenges/build-app-gemini-imagen/challenges.py
+++ cloudskillboost_challenges/build-app-gemini-imagen/challenges.py
@@ -1,12 +1,26 @@
+"""
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import vertexai
 from vertexai.preview.vision_models import ImageGenerationModel, ImageGenerationResponse
 from typing import Iterator
 from vertexai.generative_models import GenerativeModel, Part, Image
-import time
 
-def generate_bouquet_image(
-    output_file: str, prompt: str
-) -> ImageGenerationResponse:
+
+def generate_bouquet_image(output_file: str, prompt: str) -> ImageGenerationResponse:
     """Generate an AI-powered image based on a text description.
 
     This function uses Vertex AI's Image Generation model to create an image
@@ -34,6 +48,7 @@ def generate_bouquet_image(
 
     return images
 
+
 def analyze_bouquet_image(image_path: str) -> Iterator[str]:
     """Analyze an image and generate birthday wishes using Gemini Pro Vision.
 
@@ -49,10 +64,11 @@ def analyze_bouquet_image(image_path: str) -> Iterator[str]:
     model = GenerativeModel("gemini-pro-vision")
     prompt = [
         Part.from_image(Image.load_from_file(image_path)),
-        "Generate exciting birthday wishes based on the image provided"
+        "Generate exciting birthday wishes based on the image provided",
     ]
     responses = model.generate_content(prompt)
     return responses.text
+
 
 if __name__ == "__main__":
     PROJECT_ID = "your-project-id"
@@ -63,8 +79,7 @@ if __name__ == "__main__":
 
     generate_bouquet_image(
         output_file=OUTPUT_IMAGE,
-        prompt='Create an image containing a bouquet of 2 sunflowers and 3 roses',
+        prompt="Create an image containing a bouquet of 2 sunflowers and 3 roses",
     )
 
     print(analyze_bouquet_image(OUTPUT_IMAGE))
-    
