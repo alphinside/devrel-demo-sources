f629e771da9e968694d8c826fce685a3a2a97f56
Alvin Prayuda <alvinprayuda@gmail.com>
2025-01-30 13:36:16 +0700

    feat: added challenge solution

---
+++ cloudskillboost_challenges/build-app-gemini-imagen/README.md
@@ -0,0 +1,3 @@
+# Clouds Skills Boost Challenge Solution: Build Real World AI Applications with Gemini and Imagen
+
+Cloud Skill Boost: https://www.cloudskillsboost.google/course_templates/1076
---
+++ cloudskillboost_challenges/build-app-gemini-imagen/challenges.py
@@ -0,0 +1,70 @@
+import vertexai
+from vertexai.preview.vision_models import ImageGenerationModel, ImageGenerationResponse
+from typing import Iterator
+from vertexai.generative_models import GenerativeModel, Part, Image
+import time
+
+def generate_bouquet_image(
+    output_file: str, prompt: str
+) -> ImageGenerationResponse:
+    """Generate an AI-powered image based on a text description.
+
+    This function uses Vertex AI's Image Generation model to create an image
+    based on the provided text prompt. The generated image is saved to the
+    specified output file.
+
+    Args:
+        output_file (str): The local file path where the generated image will be saved.
+        prompt (str): A text description of the image to be generated.
+
+    Returns:
+        ImageGenerationResponse: The response object containing the generated image
+            and associated metadata.
+    """
+    model = ImageGenerationModel.from_pretrained("imagegeneration@002")
+
+    images = model.generate_images(
+        prompt=prompt,
+        number_of_images=1,
+        seed=1,
+        add_watermark=False,
+    )
+
+    images[0].save(location=output_file)
+
+    return images
+
+def analyze_bouquet_image(image_path: str) -> Iterator[str]:
+    """Analyze an image and generate birthday wishes using Gemini Pro Vision.
+
+    This function takes an image file, processes it using the Gemini Pro Vision model,
+    and generates creative birthday wishes based on the content of the image.
+
+    Args:
+        image_path (str): The local file path to the image to be analyzed.
+
+    Yields:
+        str: Chunks of generated birthday wishes text, streamed as they are produced.
+    """
+    model = GenerativeModel("gemini-pro-vision")
+    prompt = [
+        Part.from_image(Image.load_from_file(image_path)),
+        "Generate exciting birthday wishes based on the image provided"
+    ]
+    responses = model.generate_content(prompt)
+    return responses.text
+
+if __name__ == "__main__":
+    PROJECT_ID = "your-project-id"
+    REGION_ID = "your-location"
+    OUTPUT_IMAGE = "image.jpeg"
+
+    vertexai.init(project=PROJECT_ID, location=REGION_ID)
+
+    generate_bouquet_image(
+        output_file=OUTPUT_IMAGE,
+        prompt='Create an image containing a bouquet of 2 sunflowers and 3 roses',
+    )
+
+    print(analyze_bouquet_image(OUTPUT_IMAGE))
+    
